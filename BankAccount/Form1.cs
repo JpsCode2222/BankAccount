498bb777da986ec6bfaaef5cd7c29613b20bcc36
jayad <jayadpathandev@gmail.com>
2025-05-08 00:47:07 +0530

    Add list and contructors to store accounts and create accounts from BankAccount class

--- BankAccount/Form1.cs
+++ BankAccount/Form1.cs
@@ -8,19 +8,10 @@ namespace BankAccount
 
             // create a sample bankaccount 
             BankAccount account1 = new BankAccount("John Doe");
-            //account1.Owner = "Jayad Pathan";
-            //account1.Account_number = Guid.NewGuid();
-            //account1.Balance = 1000;
 
             BankAccount account2 = new BankAccount("Jayad Pathan");
-            //account2.Owner = "John Doe";
-            //account2.Account_number = Guid.NewGuid();
-            //account2.Balance = 10000;
 
             BankAccount account3 = new BankAccount("Tonny Stark");
-            //account3.Owner = "Tonny Stark";
-            //account3.Account_number = Guid.NewGuid();
-            //account3.Balance = 100000;
 
             List<BankAccount> bankAccounts = new List<BankAccount>();
             bankAccounts.Add(account1);
